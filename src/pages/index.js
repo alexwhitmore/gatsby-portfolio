396a81da65c9a3643bba2229354ee695fbb0fb62
alexwhitmore <heyimalexw@gmail.com>
2021-05-24 21:14:21 -0400

    Style

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet';
 import App from '../components/App';
 import { headData } from '../mock/data';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../components/layout.css';
 
 export default () => {
   return (
